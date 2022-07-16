0b6dedaef1fcbebdc985fe37721bdf7c6c7c9d5f
Başaknur SAVAŞ <89778160+basaknursavas@users.noreply.github.com>
2022-07-16 22:43:38 +0300

    Add files via upload

---
+++ PrimeNumber/src/PrimeNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner ;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int count = 0;
+        for (int i = 2 ; i <= 100 ; i++) {
+            count = 0;
+            for (int j = 2 ; j <= i ; j++) {
+                if (i % j == 0 ) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
